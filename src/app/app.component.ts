3739251a004e24cd0ba0c47803b9b150a6f9d181
Goutamsingla01 <122777044+Goutamsingla01@users.noreply.github.com>
2024-09-26 03:49:29 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './mycomponents/header/header.component';
+import { FooterComponent } from "./mycomponents/footer/footer.component";
+import { Blog1Component } from "./mycomponents/blog1/blog1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, Blog1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Blog1Component } from './mycomponents/blog1/blog1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'lessons-learned',
+        component: Blog1Component, // this is the component with the <router-outlet> in the template
+      },
+  { path: '',   redirectTo: '/lessons-learned', pathMatch: 'full' },
+  { path: '**',   redirectTo: '/lessons-learned', pathMatch: 'full' },
+];
